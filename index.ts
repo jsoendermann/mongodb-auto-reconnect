b36ac45ca01bf35b6b5e4f5ff37b5464e498e873
Jan Soendermann <jan.soendermann+git@gmail.com>
2020-08-30 23:53:25 +0100

    v2

--- index.ts
+++ index.ts
@@ -1,33 +1,35 @@
-import { Db, MongoClient } from 'mongodb'
+export type GetDb<DB> = () => Promise<DB>
 
-export default (mongoUrl: string): (() => Promise<Db>) => {
-  let dbPromise: Promise<Db> | null = null
+export interface MongoClient<DB> {
+  new (url: string, options: { autoReconnect: boolean }): MongoClient<DB>
+  db: (dbName: string) => DB
+  connect: (callback: (err: Error) => void) => void
+}
 
-  return (): Promise<Db> => {
-    if (dbPromise) {
-      return dbPromise
-    }
+export default function <DB>(
+  mongoClientClass: MongoClient<DB>,
+  mongoUrl: string,
+  dbName: string
+): GetDb<DB> {
+  const client = new mongoClientClass(mongoUrl, {
+    autoReconnect: true,
+  })
 
-    dbPromise = MongoClient.connect(mongoUrl)
-      .then((db: Db) => {
-        if (!db) {
-          dbPromise = null
-          throw new Error('db is falsy')
-        }
+  let isConnectedToDb = false
 
-        db.on('error', () => {
-          dbPromise = null
+  return () =>
+    new Promise((res, rej) => {
+      if (isConnectedToDb) {
+        res(client.db(dbName))
+      } else {
+        client.connect(err => {
+          if (err) {
+            rej(err)
+          } else {
+            isConnectedToDb = true
+            res(client.db(dbName))
+          }
         })
-        db.on('close', () => {
-          dbPromise = null
-        })
-
-        return db
-      })
-      .catch(error => {
-        dbPromise = null
-        return Promise.reject(error)
-      })
-    return dbPromise
-  }
+      }
+    })
 }
